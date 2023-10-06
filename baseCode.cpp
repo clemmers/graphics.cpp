d04072730297d308858a3cce4fc6907a371035e7
chris clem <chrisclem244@yahoo.com>
2023-10-05 18:10:00 -0700

    changes!

--- baseCode.cpp
+++ baseCode.cpp
@@ -78,6 +78,8 @@ protected:
     Color color;
 
 public:
+
+    virtual void draw(SDL_Renderer* renderer) const = 0;
     twoDimensionalObject(long double xx, long double yy, Color colorr)
 	: x(xx), y(yy), color(colorr) {}
 
@@ -107,6 +109,8 @@ public:
     }
 };
 
+
+
 class Polygon : public twoDimensionalObject {
 protected:
 	std::vector<std::vector<long double>> vertices;
@@ -121,13 +125,26 @@ public:
         return &vertices;
     }
 
-	void draw(SDL_Renderer* renderer) {
+	void draw(SDL_Renderer* renderer) const override {
 		int i;
 		for(i = 0; i < vertices.size() - 1; i++) {
 			drawLine(renderer, vertices[i][0] + x, vertices[i][1] + y, vertices[i+1][0] + x, vertices[i+1][1] + y, color);
 		}
 		drawLine(renderer, vertices[i][0] + x, vertices[i][1] + y, vertices[0][0] + x, vertices[0][1] + y, color);
 	}
+
+    void rotate(long double angle) {
+
+        long double rotationMatrix[2][2] = {
+            { cos(angle), -sin(angle) },
+            { sin(angle),  cos(angle) }
+        };
+
+        for(int i = 0; i < vertices.size(); i++) {
+            vertices[i][0] = vertices[i][0] * rotationMatrix[0][0] + vertices[i][1] * rotationMatrix[0][1];
+            vertices[i][1] = vertices[i][0] * rotationMatrix[1][0] + vertices[i][1] * rotationMatrix[1][1];
+        }
+    }
 };
 
 class RegularPolygon : public Polygon {
@@ -148,106 +165,133 @@ public:
     }
 };
 
-void setPixel(SDL_Renderer* renderer, long double x, long double y, Color color)
-{
+void setPixel(SDL_Renderer* renderer, long double x, long double y, Color color) {
 
     SDL_SetRenderDrawColor(renderer, color.getRed(), color.getGreen(), color.getBlue(), SDL_ALPHA_OPAQUE);
     SDL_RenderDrawPoint(renderer, (int) x, (int) y);
 }
 
 
+class GraphicsWindow {
+private:
+    int screenWidth;
+    int screenHeight;
+    std::vector<twoDimensionalObject*> objects;
+    Color backgroundColor;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    Uint32 prevTicks;
+    bool quit;
+    SDL_Event e;
+    int frameCount;
+    std::string windowTitle;
+
+
+public:
+    GraphicsWindow(int screenWidthInput, int screenHeightInput, std::vector<twoDimensionalObject*> objectss, Color backgroundColorInput)
+    : screenWidth(screenWidthInput), screenHeight(screenHeightInput), objects(objectss), backgroundColor(backgroundColorInput) {}
+
+    GraphicsWindow(int screenWidthInput, int screenHeightInput, std::vector<twoDimensionalObject*> objectss)
+    : screenWidth(screenWidthInput), screenHeight(screenHeightInput), objects(objectss), backgroundColor(Color()) {}
+
+    bool isOpen() {
+        return !quit;
+    }
+
+    int open() {
+        if (SDL_Init(SDL_INIT_VIDEO) < 0)
+        {
+            std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
+            return 1;
+        }
+
+        window = SDL_CreateWindow("c++ graphics - fps:", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
+        if (!window)
+        {
+            std::cerr << "SDL window creation failed: " << SDL_GetError() << std::endl;
+            return 1;
+        }
+
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if (!renderer)
+        {
+            std::cerr << "SDL renderer creation failed: " << SDL_GetError() << std::endl;
+            return 1;
+        }
+
+        
+        quit = false;
+        
+        prevTicks = SDL_GetTicks();
+        frameCount = 0;
+        windowTitle = "fps";
+
+        return 0;
+    }
+
+    int refresh() {
+        if (!quit) {
+            SDL_SetRenderDrawColor(renderer, backgroundColor.getRed(), backgroundColor.getGreen(), backgroundColor.getBlue(), 255);
+            SDL_RenderClear(renderer);
+            while (SDL_PollEvent(&e) != 0) {
+                if (e.type == SDL_QUIT) {
+                    quit = true;
+                }
+            }
+
+            for(int i = 0; i < objects.size(); i++) {
+                objects[i]->draw(renderer);
+            }
+
+            SDL_RenderPresent(renderer);
+
+            Uint32 currentTicks = SDL_GetTicks();
+            frameCount++;
+            if (currentTicks - prevTicks >= 1000) {
+                int fps = frameCount * 1000 / (currentTicks - prevTicks);
+                windowTitle = "c++ graphics - fps: " + std::to_string(fps);
+                SDL_SetWindowTitle(window, windowTitle.c_str());
+                
+                frameCount = 0;
+                prevTicks = currentTicks;
+            }
+
+            return 0;
+        }
+
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+
+        return 1;
+    }
+
+    void destroy() {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
+};
+
 
 int main()
 {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
-        return 1;
-    }
 
-    SDL_Window* window = SDL_CreateWindow("c++ graphics", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (!window)
-    {
-        std::cerr << "SDL window creation failed: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer)
-    {
-        std::cerr << "SDL renderer creation failed: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-
-    SDL_Event e;
-    bool quit = false;
-	long double x = 400.0L;
-	long double y = 400.0L;
-    int greenAmount = 0;
     std::vector<std::vector<long double>> verts {
-        {0.0L, 0.0L},
-        {0.0L, -50.0L},
-        {-50.0L, -50.0L},
-        {-50.0L, 0.0L}
-    };
+            {0.0L, 0.0L},
+            {0.0L, -50.0L},
+            {-50.0L, -50.0L},
+            {-50.0L, 0.0L}
+        };
     Color RED = Color(255,0,0);
-	Polygon test(200.0L, 200.0L, verts, RED);
+    Polygon test(200.0L, 200.0L, verts, RED);
     RegularPolygon hexagon(300, 300, 8, 50, Color(0, 0, 255));
-
-    Uint32 prevTicks = SDL_GetTicks();
-    int frameCount = 0;
-    std::string windowTitle = "fps";
-
-    while (!quit)
-    {
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-        SDL_RenderClear(renderer);
-        while (SDL_PollEvent(&e) != 0)
-        {
-            if (e.type == SDL_QUIT)
-            {
-                quit = true;
-            }
-        }
-        
-        drawLine(renderer, 400.0L, 400.0L, 410.0L, 490.0L, RED);
-		drawLine(renderer, 400.0L, 400.0L, 500.0L, 400.0L, Color(255,255,0));
-
-        if(greenAmount == 255) {
-            greenAmount = 0;
-        } greenAmount++;
-        
-		
-        test.getColorPointer()->setGreen(greenAmount);
-        hexagon.getColorPointer()->setGreen(greenAmount);
-		test.draw(renderer);
-        hexagon.draw(renderer);
-        if(hexagon.getX() > SCREEN_WIDTH) {
-            std::cout << (0 - 2*hexagon.getCircumradius());
-            hexagon.setX(0 - 2*hexagon.getCircumradius());
-        } hexagon.transform(1,0);
-        
-
-		SDL_RenderPresent(renderer);
-
-        Uint32 currentTicks = SDL_GetTicks();
-        frameCount++;
-        if (currentTicks - prevTicks >= 1000)
-        {
-            int fps = frameCount * 1000 / (currentTicks - prevTicks);
-            windowTitle = "fps: " + std::to_string(fps);
-            SDL_SetWindowTitle(window, windowTitle.c_str());
-            
-            frameCount = 0;
-            prevTicks = currentTicks;
-        }
-
+    std::vector<twoDimensionalObject*> objects{&test, &hexagon};
+    GraphicsWindow window(800, 600, objects);
+    
+    window.open();
+    while(window.isOpen()) {
+        window.refresh();
+        hexagon.rotate(0.001);
     }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
 }
