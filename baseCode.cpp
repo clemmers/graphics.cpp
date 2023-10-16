d4acb1285ed2e8b009165d9db0d754e74ca70f83
chris clem <chrisclem244@yahoo.com>
2023-10-15 21:26:47 -0700

    3d rotation and orthogonal prjection and made code very poorly organized but will fix that ltr

--- baseCode.cpp
+++ baseCode.cpp
@@ -9,12 +9,17 @@
 #include <chrono>
 
 #define PI 3.141592653589793238462643383279502884L
+#define LEFT -1
+#define UP -1
+#define RIGHT 1
+#define DOWN 1
 
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
 class GraphicsWindow;
+class Polygon;
 
 class Color {
 private:
@@ -47,7 +52,7 @@ public:
         return blue;
     }
 
-    void setRed(int r) {
+    void setRed(int red) {
         this->red = red;
     }
 
@@ -60,38 +65,54 @@ public:
     }
 };
 
-std::vector<std::vector<long double>> calculateRegularPolygonVertices (int numSides, long double circumradius) {
-    std::vector<std::vector<long double>> vertices {};
+std::vector<std::array<long double, 3>> calculateRegularPolygonVertices (int numSides, long double circumradius) {
+    std::vector<std::array<long double, 3>> vertices {};
         long double angle = 2 * PI / numSides;
         for (int i = 0; i < numSides; i++)
         {
-            vertices.push_back(std::vector<long double>{circumradius * cos(i * angle), circumradius * sin(i * angle)});
+            vertices.push_back(std::array<long double, 3>{circumradius * cos(i * angle), circumradius * sin(i * angle), 0});
         }
         return vertices;
 }
 
-class TwoDimensionalObject {
+class GraphicalObject {
 protected:
 	long double x;
 	long double y;
-    Color color;
+    long double z;
+    long double roll;
+    long double pitch;
+    long double yaw;
 
 public:
 
-    virtual void draw(GraphicsWindow* graphics) const = 0;
-    TwoDimensionalObject(long double xx, long double yy, Color colorr)
-	: x(xx), y(yy), color(colorr) {}
+    //virtual void draw(GraphicsWindow* graphics) = 0;
+    //virtual std::vector<std::array<long double, 3>>* getVertices() = 0;
+    GraphicalObject(long double x = 0, long double y = 0, long double z = 0, long double roll = 0, long double pitch = 0, long double yaw = 0)
+	: x(x), y(y), z(z), roll(roll), pitch(pitch), yaw(yaw) {}
 
-    Color* getColorPointer() {
-        return &color;
-    }
-
-    int getX() const {
+    long double getX() const {
         return x;
     }
     
-    int getY() const {
+    long double getY() const {
         return y;
+    }
+
+    long double getZ() const {
+        return z;
+    }
+
+    long double getYaw() const {
+        return yaw;
+    }
+    
+    long double getPitch() const {
+        return pitch;
+    }
+
+    long double getRoll() const {
+        return roll;
     }
 
     void setX(long double x) {
@@ -102,70 +123,145 @@ public:
         this->y = y;
     }
 
-    void transform(long double dx, long double dy) {
+    void setZ(long double z) {
+        this->z = z;
+    }
+
+    void setYaw(long double yaw) {
+        this->yaw = yaw;
+    }
+    
+    void setPitch(long double pitch) {
+        this->pitch = pitch;
+    }
+
+    void setRoll(long double roll) {
+        this->roll = roll;
+    }
+
+    void translateRoll(long double dRoll) {
+        //std::cout << roll << std::endl;
+        roll += dRoll;
+        // new slang when you notice the stripes!
+    }
+
+    void translatePitch(long double dPitch) {
+        pitch += dPitch;
+    }
+
+    void translateYaw(long double dYaw) {
+        yaw += dYaw;
+    }
+
+    void translate(long double dx, long double dy, long double dz) {
         x += dx;
         y += dy;
+        z += dz;
     }
 };
 
-class Camera {
+class Camera : public GraphicalObject {
 private:
-    std::array<long double, 3> XYZ;
-    std::array<long double, 3> rollPitchYaw;
-    std::array<long double, 3> vXYZ;
-    std::array<long double, 3> vRollPitchYaw;
+    long double vX;
+    long double vY;
+    long double vZ;
     long double friction;
+    long double movementSpeed;
+    long double lookSpeed;
     std::chrono::time_point<std::chrono::system_clock> timeWhenLastUpdated;
-    long double speed;
 
 public:
-    Camera(const std::array<long double, 3>& XYZ = {},
-           const std::array<long double, 3>& rollPitchYaw = {},
-           const std::array<long double, 3>& vXYZ = {},
-           const std::array<long double, 3>& vRollPitchYaw = {},
-           const std::array<long double, 3>& aRollPitchYaw = {},
-           long double friction = 0.5,
-           long double speed = 1)
-        : rollPitchYaw(rollPitchYaw),
-          friction(friction),
-          vRollPitchYaw(vRollPitchYaw),
-          XYZ(XYZ), vXYZ(vXYZ), speed(speed),
+    Camera(long double x = 0, long double y = 0, long double z = 0,
+           long double roll = 0, long double pitch = 0, long double yaw = 0,
+           long double movementSpeed = 1, long double lookSpeed = 1.0L/(2*PI),
+           long double friction = 0.5, long double vX = 0, long double vY = 0,
+           long double vZ = 0)
+        : GraphicalObject(x,y,z,roll,pitch,yaw), friction(friction),
+          movementSpeed(movementSpeed), lookSpeed(lookSpeed),
+          vX(vX), vY(vY), vZ(vZ),
           timeWhenLastUpdated(std::chrono::high_resolution_clock::now()) {}
 
     void update() {
         long double timeSinceLastFrame = std::chrono::duration_cast<std::chrono::nanoseconds>
             (std::chrono::high_resolution_clock::now() - timeWhenLastUpdated).count() / 100000000.0L;
-        XYZ[0] += vXYZ[0] * timeSinceLastFrame;
-        XYZ[1] += vXYZ[1] * timeSinceLastFrame;
-        XYZ[2] += vXYZ[2] * timeSinceLastFrame;
-        vXYZ[0] *= pow(friction, timeSinceLastFrame);
-        vXYZ[1] *= pow(friction, timeSinceLastFrame);
-        vXYZ[2] *= pow(friction, timeSinceLastFrame);
+        x += vX * timeSinceLastFrame;
+        y += vY * timeSinceLastFrame;
+        z += vZ * timeSinceLastFrame;
+        vX *= pow(friction, timeSinceLastFrame);
+        vY *= pow(friction, timeSinceLastFrame);
+        vZ *= pow(friction, timeSinceLastFrame);
         timeWhenLastUpdated = std::chrono::high_resolution_clock::now();
     }
 
-    long double getX() const {
-        return XYZ[0];
-    }
-
-    long double getY() const {
-        return XYZ[1];
-    }
-
-    long double getZ() const {
-        return XYZ[2];
-    }
-
     void translateVX(int direction) {
-        vXYZ[0]+=direction*speed;
+        vX += direction * movementSpeed;
     }
     
     void translateVY(int direction) {
-        vXYZ[1]+=direction*speed;
+        vY += direction * movementSpeed;
     }
 
     void translateVZ(int direction) {
-        vXYZ[2]+=direction*speed;
+        vZ += direction * movementSpeed;
+    }
+
+    void translateLook(int x, int y) {
+        translateYaw(x * lookSpeed);
+        translatePitch(y * lookSpeed);
+    }
+
+};
+
+std::array<long double, 3> rotateX(long double x, long double y, long double z, long double angle) {
+    std::array<long double, 3> rotatedVector {
+        x,
+        y * cos(angle) + z * -sin(angle),
+        y * sin(angle) + z * cos(angle)
+    };
+
+    return rotatedVector;
+}
+
+std::array<long double, 3> rotateY(long double x, long double y, long double z, long double angle) {
+    std::array<long double, 3> rotatedVector {
+        x * cos(angle) + z * sin(angle),
+        y,
+        x * -sin(angle) + z * cos(angle)
+    };
+
+    return rotatedVector;
+}
+
+std::array<long double, 3> rotateZ(long double x, long double y, long double z, long double angle) {
+    std::array<long double, 3> rotatedVector {
+        x * cos(angle) + y * -sin(angle),
+        x * sin(angle) + y * cos(angle),
+        z
+    };
+
+    return rotatedVector;
+}
+
+
+class Polygon : public GraphicalObject {
+protected:
+	std::vector<std::array<long double, 3>> vertices;
+    Color color;
+
+public:
+	Polygon(long double x = 0, long double y = 0, long double z = 0, std::vector<std::array<long double, 3>> vertices = {}, Color color = Color(), long double roll = 0, long double pitch = 0, long double yaw = 0)
+    : GraphicalObject(x, y, z, roll, pitch, yaw), vertices(vertices), color(color) {}
+
+
+    void draw(GraphicsWindow& graphics);
+
+
+    Color& getColor() {
+        return color;
+    }
+
+    std::vector<std::array<long double, 3>>& getVertices() {
+        return vertices;
     }
 };
 
@@ -174,7 +270,7 @@ class GraphicsWindow {
 private:
     int screenWidth;
     int screenHeight;
-    std::vector<TwoDimensionalObject*> objects;
+    std::vector<Polygon*>* objects;
     Color backgroundColor;
     SDL_Window* window;
     SDL_Renderer* renderer;
@@ -184,17 +280,34 @@ private:
     int frameCount;
     std::string windowTitle;
     Camera camera;
+    int prevMouseX;
+    int prevMouseY;
 
 public:
-    GraphicsWindow(int screenWidthInput, int screenHeightInput, std::vector<TwoDimensionalObject*> objectss, Color backgroundColorInput, Camera cameraInput)
-    : screenWidth(screenWidthInput), screenHeight(screenHeightInput), objects(objectss), backgroundColor(backgroundColorInput), camera(cameraInput) {}
+    GraphicsWindow(int screenWidth, int screenHeight, std::vector<Polygon*>* objects, Color backgroundColor, Camera camera)
+    : screenWidth(screenWidth), screenHeight(screenHeight), objects(objects), backgroundColor(backgroundColor), camera(camera), prevMouseX(0), prevMouseY(0) {}
 
-    GraphicsWindow(int screenWidthInput, int screenHeightInput, std::vector<TwoDimensionalObject*> objectss)
-    : screenWidth(screenWidthInput), screenHeight(screenHeightInput), objects(objectss), backgroundColor(Color()), camera(Camera()) {}
 
-    void drawLine(long double x1, long double y1, long double x2, long double y2, Color color) {
+    void drawLine(long double x1, long double y1, long double z1, long double x2, long double y2, long double z2, Color& color) {
         SDL_SetRenderDrawColor(renderer, color.getRed(), color.getGreen(), color.getBlue(), SDL_ALPHA_OPAQUE);
         SDL_RenderDrawLine(renderer, (int) (x1 - camera.getX()), (int) (y1 - camera.getY()), (int) (x2 - camera.getX()), (int) (y2 - camera.getY()));
+        //SDL_RenderDrawLine(renderer, (int) ((x1 - camera.getX())/(z1 - camera.getZ())), (int) ((y1 - camera.getY())/(z1 - camera.getZ())), (int) ((x2 - camera.getX())/(z2 - camera.getZ())), (int) ((y2 - camera.getY())/(z2 - camera.getZ())));
+    }
+
+    void drawObject(Polygon& object) {
+        std::vector<std::array<long double, 3>> rotatedVertices{};
+        std::vector<std::array<long double, 3>>& verticesPointer = object.getVertices();
+        for(int i = 0; i < verticesPointer.size(); i++) {
+            std::array<long double, 3> tempVerts = rotateX(verticesPointer[i][0],verticesPointer[i][1],verticesPointer[i][2],object.getRoll());
+            tempVerts = rotateY(tempVerts[0],tempVerts[1],tempVerts[2],object.getPitch());
+            tempVerts = rotateZ(tempVerts[0],tempVerts[1],tempVerts[2],object.getYaw());
+            rotatedVertices.push_back(tempVerts);
+        }
+        for(int i = 0; i < rotatedVertices.size(); i++) {
+            for(int j = 0; j < rotatedVertices.size(); j++) {
+                this->drawLine(rotatedVertices[i][0] + object.getX(), rotatedVertices[i][1] + object.getY(), rotatedVertices[i][2] + object.getZ(), rotatedVertices[j][0] + object.getX(), rotatedVertices[j][1] + object.getY(), rotatedVertices[j][2] + object.getZ(), object.getColor());
+            }
+        }
     }
 
     bool isOpen() {
@@ -243,25 +356,30 @@ public:
             if (e.type == SDL_QUIT) {
                 quit = true;
             }
+            if (e.type == SDL_MOUSEMOTION) {
+                camera.translateLook(prevMouseX - e.motion.x, prevMouseY - e.motion.y);
+                prevMouseX = e.motion.x;
+                prevMouseY = e.motion.y;
+            }
         }
 
         const Uint8* keystates = SDL_GetKeyboardState(NULL);
 
         if(keystates[SDL_SCANCODE_LEFT]) {
-            camera.translateVX(-1);
+            camera.translateVX(LEFT);
         }
         if(keystates[SDL_SCANCODE_RIGHT]) {
-            camera.translateVX(1);
+            camera.translateVX(RIGHT);
         }
         if(keystates[SDL_SCANCODE_UP]) {
-            camera.translateVY(-1);
+            camera.translateVY(UP);
         }
         if(keystates[SDL_SCANCODE_DOWN]) {
-            camera.translateVY(1);
+            camera.translateVY(DOWN);
         }
         
-        for(int i = 0; i < objects.size(); i++) {
-            objects[i]->draw(this);
+        for(int i = 0; i < objects->size(); i++) {
+            (*objects)[i]->draw(*this);
         }
 
         camera.update();
@@ -289,42 +407,9 @@ public:
 };
 
 
-
-class Polygon : public TwoDimensionalObject {
-protected:
-	std::vector<std::vector<long double>> vertices;
-
-public:
-	Polygon(long double x, long double y, std::vector<std::vector<long double>> verticess, Color color)
-    : TwoDimensionalObject(x, y, color), vertices(verticess) {}
-
-    Polygon(std::vector<std::vector<long double>> verticess) : TwoDimensionalObject(0.0L, 0.0L, Color()), vertices(verticess) {}
-
-    std::vector<std::vector<long double>>* getVerticesPointer() {
-        return &vertices;
-    }
-
-	void draw(GraphicsWindow* graphics) const override {
-		int i;
-		for(i = 0; i < vertices.size() - 1; i++) {
-			graphics->drawLine(vertices[i][0] + x, vertices[i][1] + y, vertices[i+1][0] + x, vertices[i+1][1] + y, color);
-		}
-		graphics->drawLine(vertices[i][0] + x, vertices[i][1] + y, vertices[0][0] + x, vertices[0][1] + y, color);
+void Polygon::draw(GraphicsWindow& graphics) {
+		graphics.drawObject(*this);
 	}
-
-    void rotate(long double angle) {
-
-        long double rotationMatrix[2][2] = {
-            { cos(angle), -sin(angle) },
-            { sin(angle),  cos(angle) }
-        };
-
-        for(int i = 0; i < vertices.size(); i++) {
-            vertices[i][0] = vertices[i][0] * rotationMatrix[0][0] + vertices[i][1] * rotationMatrix[0][1];
-            vertices[i][1] = vertices[i][0] * rotationMatrix[1][0] + vertices[i][1] * rotationMatrix[1][1];
-        }
-    }
-};
 
 class RegularPolygon : public Polygon {
 protected:
@@ -332,8 +417,8 @@ protected:
     long double circumradius;
 
 public:
-    RegularPolygon(long double x, long double y, int numSidess, long double circumradiuss, Color color)
-    : numSides(numSidess), circumradius(circumradiuss), Polygon(x, y, calculateRegularPolygonVertices(numSidess, circumradiuss), color) {}
+    RegularPolygon(long double x = 0, long double y = 0, long double z = 0, int numSides = 5, long double circumradius = 50, Color color = Color())
+    : numSides(numSides), circumradius(circumradius), Polygon(x, y, z, calculateRegularPolygonVertices(numSides, circumradius), color) {}
 
     long double getCircumradius() const {
         return circumradius;
@@ -342,6 +427,12 @@ public:
     int getNumSides() const {
         return numSides;
     }
+};
+
+class Polyhedron : public Polygon {
+public:
+    Polyhedron(long double x = 0, long double y = 0, long double z = 0, std::vector<std::array<long double, 3>> vertices = {}, long double yaw = 0, long double pitch = 0, long double roll = 0, Color color = Color())
+    : Polygon(x,y,z,vertices,color,roll,pitch,yaw) {}
 };
 
 void setPixel(SDL_Renderer* renderer, long double x, long double y, Color color) {
@@ -353,21 +444,37 @@ void setPixel(SDL_Renderer* renderer, long double x, long double y, Color color)
 int main()
 {
 
-    std::vector<std::vector<long double>> verts {
-            {0.0L, 0.0L},
-            {0.0L, -50.0L},
-            {-50.0L, -50.0L},
-            {-50.0L, 0.0L}
-        };
+    std::vector<std::array<long double, 3>> cubeVerts {
+        {-50.0L, -50.0L, -50.0L},
+        {50.0L, -50.0L, -50.0L},
+        {50.0L, 50.0L, -50.0L},
+        {-50.0L, 50.0L, -50.0L},
+        {-50.0L, -50.0L, 50.0L},
+        {50.0L, -50.0L, 50.0L},
+        {50.0L, 50.0L, 50.0L},
+        {-50.0L, 50.0L, 50.0L}
+    };
+    std::vector<std::array<long double, 3>> squareVerts {
+            {0.0L, 0.0L, 0.0L},
+            {0.0L, -50.0L, 0.0L},
+            {-50.0L, -50.0L, 0.0L},
+            {-50.0L, 0.0L, 0.0L}
+    };
     Color RED = Color(255,0,0);
-    Polygon test(200.0L, 200.0L, verts, RED);
-    RegularPolygon hexagon(300, 300, 8, 50, Color(0, 255, 255));
-    std::vector<TwoDimensionalObject*> objects{&test, &hexagon};
-    GraphicsWindow window(800, 600, objects, Color(255,255,255), Camera());
+    Polygon test(200.0L, 200.0L, 200.0L, squareVerts, RED);
+    Polyhedron cube(400.0L, 400.0L, 100.0L, cubeVerts);
+    RegularPolygon hexagon(300, 300, 200, 8, 50, Color(0, 255, 255));
+    std::vector<Polygon*> objects{&test, &hexagon, &cube};
+    GraphicsWindow window(800, 600, &objects, Color(255,255,255), Camera());
     
     window.open();
+
     while(window.isOpen()) {
+        test.translateRoll(0.001);
+        cube.translateRoll(0.001);
+        cube.translatePitch(0.001);
+        cube.translateYaw(0.001);
+        cube.translate(0.1,0,0);
         window.refresh();
-        hexagon.rotate(0.001);
     }
 }
